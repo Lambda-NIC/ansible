bd9525ba1bc3c9a2375c426b62b23b881ca81663
Muhammad Shahbaz <muhammad.shahbaz@stanford.edu>
2018-12-04 02:00:49 -0800

    upgraded to k8s version 1.8.10

---
+++ testbed/stanford/cleanup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# removes kubernetes dashboard
+ansible-playbook kubernetes.yml --tags destroy_apps
+
+ansible-playbook kubernetes.yml --tags destroy_cluster
+ansible-playbook kubernetes.yml --tags remove_common
+
--- testbed/stanford/setup.sh
+++ testbed/stanford/setup.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
 ansible-playbook kubernetes.yml --tags install_common
+ansible-playbook kubernetes.yml --tags create_cluster
+
+ansible-playbook kubernetes.yml --tags create_apps
