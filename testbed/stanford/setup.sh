57ecf624d17f67dd731c920f0512be02a264bb06
Muhammad Shahbaz <muhammad.shahbaz@stanford.edu>
2018-12-04 02:09:14 -0800

    nits

--- testbed/stanford/setup.sh
+++ testbed/stanford/setup.sh
@@ -3,4 +3,4 @@
 ansible-playbook kubernetes.yml --tags install_common
 ansible-playbook kubernetes.yml --tags create_cluster
 
-ansible-playbook kubernetes.yml --tags create_apps
+#ansible-playbook kubernetes.yml --tags create_apps
