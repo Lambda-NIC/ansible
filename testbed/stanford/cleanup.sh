0f353c27fbfb4ef49aabb971659f183132fd7a5a
Sean Choi <schoi@fb.com>
2018-12-04 14:30:54 -0800

    Committing changes up to the latest update

--- testbed/stanford/cleanup.sh
+++ testbed/stanford/cleanup.sh
@@ -1,8 +1,7 @@
-#!/usr/bin/env bash
+e!/usr/bin/env bash
 
 # removes kubernetes dashboard
 #ansible-playbook kubernetes.yml --tags destroy_apps
 
 ansible-playbook kubernetes.yml --tags destroy_cluster
 ansible-playbook kubernetes.yml --tags remove_common
-
