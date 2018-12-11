ba67bfc146889ac8c24006102b9ca4f51ede8758
Sean Choi <yo2seol@gmail.com>
2018-12-11 14:06:11 -0800

    Adding more tools

---
+++ tools/create_dashboard_sa.sh
@@ -0,0 +1,4 @@
+kubectl create serviceaccount dashboard -n default
+kubectl create clusterrolebinding dashboard-admin -n default \
+  --clusterrole=cluster-admin \
+  --serviceaccount=default:dashboard
